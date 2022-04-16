7095ebaa88700bab223a990c776d9e88be2d682a
wsydn <wsydney@web.de>
2022-04-16 10:14:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Craft 4 Empty Starter
+
+This is a pure Craft CMS starter as provided in the craftcms/craft package with 
+the following additions:
+
+* Added modules/_faux to enable autocompletion for some most frequently used variables in twig
+* Set system timezone to Europe/Berlin
+* Added /web/cpresources, /node_modules to .gitignore
+* Added some settings to config/general.php
+* Added config/redactor/Custom.json as a safe Redactor config
+* Added log component settings to config/app.php to avoid exhaustive logging
---
+++ modules/_faux/FauxTwigExtension.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * FauxTwigExtension for Craft CMS 3.x
+ *
+ * This is intended to be used with the Symfony Plugin for PhpStorm:
+ * https://plugins.jetbrains.com/plugin/7219-symfony-plugin
+ *
+ * It will provide full auto-complete for craft.app. and and many other useful things
+ * in your Twig templates.
+ *
+ * Place the file somewhere in your project or include it via PhpStorm Settings -> Include Path.
+ * You never call it, it's never included anywhere via PHP directly nor does it affect other
+ * classes or Twig in any way. But PhpStorm will index it, and think all those variables
+ * are in every single template and thus allows you to use Intellisense auto completion.
+ *
+ * Thanks to Robin Schambach; for context, see:
+ * https://github.com/Haehnchen/idea-php-symfony2-plugin/issues/1103
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2019 nystudio107
+ */
+
+namespace modules\_faux;
+
+//use craft\commerce\elements\Order;
+//use craft\commerce\elements\Product;
+//use craft\commerce\elements\Variant;
+//use craft\commerce\models\LineItem;
+//use craft\commerce\Plugin;
+
+use craft\elements\Asset;
+use craft\elements\Category;
+use craft\elements\Entry;
+use craft\elements\GlobalSet;
+use craft\elements\MatrixBlock;
+use craft\elements\Tag;
+use craft\elements\User;
+use craft\models\Site;
+use craft\web\twig\variables\Paginate;
+use Illuminate\Support\Collection;
+use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
+
+/**
+ * @author    nystudio107
+ * @package   FauxTwigExtension
+ * @since     1.0.0
+ */
+class FauxTwigExtension extends AbstractExtension implements GlobalsInterface
+{
+    public function getGlobals(): array
+    {
+        return [
+            // Craft Elements
+            'element' => new Entry(),
+            'asset' => new Asset(),
+            'block' => new MatrixBlock(),
+            'image' => new Asset(),
+            'category' => new Category(),
+            'tag' => new Tag(),
+            'entry' => new Entry(),
+            'draft' => new Entry(),
+            'siteInfo' => new GlobalSet(),
+
+            'entries' => new Collection(),
+            'images' => new Collection(),
+
+            'currentUser' => new User(),
+            'currentSite' => new Site(),
+
+            'pageInfo' => new Paginate(),
+
+            // Commerce Elements
+            //'lineItem' => new LineItem(),
+            //'order' => new Order(),
+            //'product' => new Product(),
+            //'variant' => new Variant(),
+            //'commerce' => new Plugin(),
+
+
+            // Third party globals
+            //'seomatic' => new \nystudio107\seomatic\variables\SeomaticVariable(),
+        ];
+    }
+}
